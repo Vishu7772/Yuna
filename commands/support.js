07028c00f7dbf1db62905812990bfb4372503371
Vishu7772 <162317231+Vishu7772@users.noreply.github.com>
2024-08-29 06:44:18 +0530

    Update support.js

--- commands/support.js
+++ commands/support.js
@@ -30,18 +30,18 @@ module.exports = {
     try {
 
       const supportServerLink = "https://discord.gg/xQF9f9yUEM";
-      const githubLink = "https://github.com/GlaceYT";
-      const replitLink = "https://replit.com/@GlaceYT";
-      const youtubeLink = "https://www.youtube.com/@GlaceYT";
+      const githubLink = "https://github.com/Vishu7772";
+    
+      const youtubeLink = "https://www.youtube.com/@Atrone5980";
         const embed = new EmbedBuilder()
             .setColor('#b300ff')
             .setAuthor({
               name: 'Support Server',
-              iconURL: 'https://cdn.discordapp.com/attachments/1230824451990622299/1230824519220985896/6280-2.gif?ex=6638ae28&is=66375ca8&hm=13e4a1b91a95b2934a39de1876e66c11711c7b30ac1a91c2a158f2f2ed1c2fc6&', 
+              iconURL: 'https://cdn.discordapp.com/attachments/1278518568379744307/1278522712985436211/images_8.jpg?ex=66d11c90&is=66cfcb10&hm=cc86d403214f9f6006e0d7d0252cb61dc1c05f3fc11c29b912aef37c4156e136&', 
               url: 'https://discord.gg/xQF9f9yUEM'
           })
-            .setDescription(`➡️ **Join our Discord server for support and updates:**\n- Discord - ${supportServerLink}\n\n➡️ **Follow us on:**\n- GitHub - ${githubLink}\n- Replit - ${replitLink}\n- YouTube - ${youtubeLink}`)
-            .setImage('https://cdn.discordapp.com/attachments/1113800537402527903/1236803979996958740/11.png?ex=663956f7&is=66380577&hm=3b3c19a11adcb979517a133f2907f671305d23f1f5092cf7df043e6d5cab07bc&')
+            .setDescription(`➡️ **Join our Discord server for support and updates:**\n- Discord - ${supportServerLink}\n\n➡️ **Follow us on:**\n- GitHub - ${githubLink}\n- YouTube - ${youtubeLink}`)
+            .setImage('https://cdn.discordapp.com/attachments/1278518568379744307/1278522712985436211/images_8.jpg?ex=66d11c90&is=66cfcb10&hm=cc86d403214f9f6006e0d7d0252cb61dc1c05f3fc11c29b912aef37c4156e136&')
             .setTimestamp();
 
       return interaction.reply({ embeds: [embed] });
