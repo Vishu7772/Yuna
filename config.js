ff62ad62fdbe1c5d0bdd06b564519c182eb2f893
Vishu7772 <162317231+Vishu7772@users.noreply.github.com>
2024-09-02 14:36:11 +0530

    Update config.js

--- config.js
+++ config.js
@@ -23,12 +23,12 @@
 
 module.exports = {
   TOKEN: "",
-  ownerID: ["1004206704994566164", ""], 
+  ownerID: ["1187666470990266410", "913605128362295346"], 
   setupFilePath: './commands/setup.json',
   commandsDir: './commands',  
   embedColor: "#1db954",
   musicardTheme:"themes16", //Goes from themes1 to themes19
-  activityName: "You", // This is bot status Write Anything here 
+  activityName: "Pheonix", // This is bot status Write Anything here 
   activityType: "WATCHING",  // Available activity types : LISTENING , PLAYING
   SupportServer: "https://discord.gg/xQF9f9yUEM",
   CheckmarkIcon: "https://cdn.discordapp.com/attachments/1230824451990622299/1230836684774576168/7762-verified-blue.gif",
